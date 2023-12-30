a174be5c210eabc2400bd1d299b36e1192faeed6
gcmgabrielmartinez <gabrielcalvo2812@gmail.com>
2023-12-30 23:05:27 +0000

    Commiting mean and sd calculator

--- main.py
+++ main.py
@@ -4,5 +4,11 @@ from unittest import main
 
 print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
 
+#print(mean_var_std.calculate([2,6,2,8,4,0,1,5,7]))
+
+#print(mean_var_std.calculate([9,1,5,3,3,3,2,9,0]))
+
+
+
 # Run unit tests automatically
 main(module='test_module', exit=False)
